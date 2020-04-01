56fc2a5dab971e9bfcbba76c97b9633158c150cb
kalu ikechukwu e.a <swaykay.foreveralearner@gmail.com>
2020-04-01 09:52:12 +0100

    Added externalJavascriptUrls for read.js

--- src/assets/js/read.js
+++ src/assets/js/read.js
@@ -23,3 +23,57 @@ externalJavascriptFiles.map((element, index) => {
         }, 500);
     });
 });
+
+var externalJavascriptUrls = [
+    'https://code.jquery.com/jquery-3.4.1.min.js'
+]
+
+console.log(externalJavascriptUrls.length + " number of Url(s).");
+
+const getScript = (url) => {
+    return new Promise((resolve, reject) => {
+        const http      = require('http'),
+            https     = require('https');
+
+        let client = http;
+
+        if (url.toString().indexOf("https") === 0) {
+            client = https;
+        }
+
+        client.get(url, (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                resolve(
+                    setTimeout(() => {
+                        fs.appendFile('vendors.js', data.toString(), function (err) {
+                            if (err) { console.log(err) }
+                            console.log(url +" written to file.");
+                        });
+                    }, 500)
+                );
+            });
+
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+};
+
+externalJavascriptUrls.map((element, index) => {
+    (async (url) => {
+        try {
+            console.log(await getScript(url));
+        }
+        catch (error) {
+    
+        }
+    })(element);
+});
